25c66b3bff8b450f333043adbeda1a4ff21a7d46
sbk <166150577+TheMountain427@users.noreply.github.com>
2024-11-17 21:11:14 -0600

    Pan and zoom Just threw it in there, mostly works

--- ImageSorter/Views/WorkspaceView.axaml.cs
+++ ImageSorter/Views/WorkspaceView.axaml.cs
@@ -1,4 +1,6 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Controls.PanAndZoom;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using ImageSorter.ViewModels;
@@ -8,9 +10,41 @@ namespace ImageSorter.Views;
 
 public partial class WorkspaceView : ReactiveUserControl<WorkspaceViewModel>
 {
+    // This doesn't work since we aren't using : Window and InitializeComponent()
+    // because we are using Reactive and stuff
+    // Normally this would auto bind to the xaml control with the same name
+    // Have to figure out how to do this with Reactive I guess
+    private readonly ZoomBorder? _zoomBorder;
+
+    private void ZoomBorder_KeyDown(object? sender, KeyEventArgs e)
+    {
+        switch (e.Key)
+        {
+            case Key.F:
+                _zoomBorder?.Fill();
+                break;
+            case Key.U:
+                _zoomBorder?.Uniform();
+                break;
+            case Key.R:
+                _zoomBorder?.ResetMatrix();
+                break;
+            case Key.T:
+                _zoomBorder?.ToggleStretchMode();
+                _zoomBorder?.AutoFit();
+                break;
+        }
+    }
+
     public WorkspaceView()
     {
         this.WhenActivated(disposables => { });
         AvaloniaXamlLoader.Load(this);
+
+        _zoomBorder = this.Find<ZoomBorder>("ZoomBorder");
+        if (_zoomBorder != null)
+        {
+            _zoomBorder.KeyDown += ZoomBorder_KeyDown;
+        }
     }
 }
