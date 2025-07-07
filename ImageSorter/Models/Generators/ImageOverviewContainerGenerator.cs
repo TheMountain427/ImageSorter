cc15083e8c66d2e0c5463ad19fa2369072c1438c
TheMountain427 <166150577+TheMountain427@users.noreply.github.com>
2025-07-06 23:48:36 -0500

    Bulk update image filters from the overview view. Kinda, still have to click each one. I had to hack it to even get that to work. Shift clicking or dragging to select multiple is not possible without doing an immense amount of work do to the fundamental flaws of my code. For my future self, this is a finished app, it does what it set out to do even if that is not efficient. If you want changes, rewrite the app with a proper plan.

--- ImageSorter/Models/Generators/ImageOverviewContainerGenerator.cs
+++ ImageSorter/Models/Generators/ImageOverviewContainerGenerator.cs
@@ -1,24 +1,34 @@
+using System.Reactive;
 using Avalonia.Animation;
 using Avalonia.Controls;
 using Avalonia.Controls.Converters;
+using Avalonia.Data;
 using Avalonia.Layout;
 using Avalonia.Media;
 using Avalonia.Styling;
+using Avalonia.Threading;
 using ImageSorter.Models;
+using ReactiveUI;
 
 namespace ImageSorter.Models.Generators;
 
-public static class ImageOverviewContainerGenerator
+public class ImageOverviewContainerGenerator
 {
     public static IEnumerable<StackPanel> CreateImageRows(
     IEnumerable<ImageDetails> images,
     int itemsPerLine,
-    Action<ImageDetails>? onImageClick = null)
+    IObservable<bool> bulkSelectionActivated,
+    Action<ImageDetails> onImageClick,
+    ReactiveCommand<(Button, ImageDetails), Unit> onBulkImageClick)
     {
         var imageList = images as IList<ImageDetails> ?? images.ToList();
         for (int i = 0; i < imageList.Count; i += itemsPerLine)
         {
-            var rowPanel = new StackPanel { Orientation = Orientation.Horizontal, Spacing = 0 };
+            var rowPanel = new StackPanel
+            {
+                Orientation = Orientation.Horizontal,
+                Spacing = 0
+            };
 
             foreach (var img in imageList.Skip(i).Take(itemsPerLine))
             {
@@ -31,7 +41,6 @@ public static class ImageOverviewContainerGenerator
                     DataContext = img
                 };
 
-                // Animation style (same as your XAML)
                 var animation = new Animation
                 {
                     Duration = TimeSpan.FromSeconds(2),
@@ -49,51 +58,119 @@ public static class ImageOverviewContainerGenerator
                         }
                     }
                 };
+
                 var style = new Style(x => x.OfType<Border>())
                 {
                     Animations = { animation }
                 };
                 border.Styles.Add(style);
 
+                var grid = new Grid
+                {
+                    VerticalAlignment = VerticalAlignment.Stretch,
+                    HorizontalAlignment = HorizontalAlignment.Stretch
+                };
                 // Button inside border
-                var button = new Button
+                var imageButton = new Button
                 {
                     Background = Brushes.Transparent,
                     CommandParameter = img.FileName,
                     Content = new StackPanel
                     {
-                        Children =
+                        Children = {
+                    new StackPanel
                     {
-                        new TextBlock
+                        Children =
                         {
-                            Text = img.FilteredValue,
-                            FontSize = 11,
-                            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
-                            TextWrapping = TextWrapping.NoWrap
-                        },
-                        new Image
-                        {
-                            Source = img.ThumbnailBitmap,
-                            Margin = new (5),
-                            IsVisible = img.ThumbnailBitmap != null
-                        },
-                        new TextBlock
-                        {
-                            Text = img.FileName,
-                            FontSize = 11,
-                            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
-                            TextWrapping = TextWrapping.NoWrap
+                            new TextBlock
+                            {
+                                Text = img.FilteredValue,
+                                FontSize = 11,
+                                HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+                                TextWrapping = TextWrapping.NoWrap
+                            },
+                            new Image
+                            {
+                                Source = img.ThumbnailBitmap,
+                                Margin = new (5),
+                                IsVisible = img.ThumbnailBitmap != null
+                            },
+                            new TextBlock
+                            {
+                                Text = img.FileName,
+                                FontSize = 11,
+                                HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+                                TextWrapping = TextWrapping.NoWrap
+                            }
                         }
-                    }
+                    } }
                     }
                 };
-                if (onImageClick != null)
+
+                grid.Children.Add(imageButton);
+
+                border.Child = grid;
+
+                var bulkSelectButton = new Button
                 {
-                    button.Click += (_, __) => onImageClick(img);
-                }
-                border.Child = button;
+                    Background = Brushes.Transparent,
+                    IsVisible = false,
+                    VerticalAlignment = VerticalAlignment.Stretch,
+                    HorizontalAlignment = HorizontalAlignment.Stretch
+                };
+
+                bulkSelectionActivated.Subscribe(isBulk =>
+                {
+                    // UI updates must be dispatched to the UI thread
+                    Dispatcher.UIThread.Post(() =>
+                    {
+                        bulkSelectButton.IsVisible = isBulk;
+                        imageButton.IsHitTestVisible = !isBulk;
+
+                        if (!isBulk)
+                        {
+                            bulkSelectButton.Tag = "0";
+                        }
+                    });
+                });
+
+                grid.Children.Add(bulkSelectButton);
+
+                bulkSelectButton.Click += (s, e) =>
+                {
+                    if (bulkSelectButton.IsVisible)
+                    {
+                        bulkSelectButton.Tag = bulkSelectButton.Tag?.ToString() == "1" ? "0" : "1";
+                    }
+                };
+
+                bulkSelectButton.PropertyChanged += (s, e) =>
+                {
+                    if (e.Property == Button.TagProperty)
+                    {
+                        if (bulkSelectButton.Tag?.ToString() == "1")
+                        {
+                            Dispatcher.UIThread.Post(() =>
+                            {
+                                bulkSelectButton.BorderBrush = new SolidColorBrush(Colors.LimeGreen); // Selected color
+                            });
+                        }
+                        else
+                        {
+                            Dispatcher.UIThread.Post(() =>
+                            {
+                                bulkSelectButton.BorderBrush = new SolidColorBrush(Color.Parse("#FF232323")); // Default color
+                            });
+                        }
+                    }
+                };
+
+                imageButton.Click += (_, __) => onImageClick(img);
+                bulkSelectButton.Click += (_, __) => onBulkImageClick.Execute((bulkSelectButton, img)).Subscribe();
+
                 rowPanel.Children.Add(border);
             }
+
             yield return rowPanel;
         }
     }
